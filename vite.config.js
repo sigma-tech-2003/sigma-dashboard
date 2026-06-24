c7fd1d67b3f11e11beb4d2990307105873fb7249
sigma-tech-2003 <rayblack1511@gmail.com>
2026-06-25 04:27:39 +0500

    Remove manualChunks

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // 1000kb — warning hategi
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
-          react: ['react', 'react-dom'],
-        }
-      }
-    }
+    chunkSizeWarningLimit: 1000
   }
 })
