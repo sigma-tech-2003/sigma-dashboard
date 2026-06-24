cc50d5e19adb316ef3a2675a969c35bd668535ac
sigma-tech-2003 <rayblack1511@gmail.com>
2026-06-24 05:17:48 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // 1000kb — warning hategi
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+          react: ['react', 'react-dom'],
+        }
+      }
+    }
+  }
+})
